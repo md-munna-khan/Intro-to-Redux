db30733ce3c6545e0a409a8423827c6c531c8660
md.munna <mk8761174@gmail.com>
2025-06-26 09:02:33 +0600

    21-7 Set Up a Redux Store.

---
+++ intro-to-redux/src/App.tsx
@@ -0,0 +1,17 @@
+
+import './App.css'
+
+function App() {
+ 
+
+  return (
+   <div>
+    <div>Counter With Redux</div>
+    <button>Increment</button>
+    <div>0</div>
+    <button>Decrement</button>
+   </div>
+  )
+}
+
+export default App
